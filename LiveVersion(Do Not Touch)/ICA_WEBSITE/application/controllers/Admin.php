902d37c092075fd24bc9b118e8de9ebf1e2ca575
Daniel <daniel.cassar.a100531@mcast.edu.mt>
2018-05-29 14:39:33 +0200

    small changes

--- LiveVersion(Do Not Touch)/ICA_WEBSITE/application/controllers/Admin.php
+++ LiveVersion(Do Not Touch)/ICA_WEBSITE/application/controllers/Admin.php
@@ -2,6 +2,12 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends MY_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+	
     public function adminVacancyList()
     {
         $data = array(
