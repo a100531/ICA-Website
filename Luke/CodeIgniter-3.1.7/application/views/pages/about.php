7530d2d67a1d24ff6068e47e57a0e84ed6325e8a
crazymaltese <34515052+crazymaltese@users.noreply.github.com>
2018-05-30 21:57:59 +0200

    Using plugin to read pdfs to put for time tables

---
+++ Luke/CodeIgniter-3.1.7/application/views/pages/about.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        Pls work god dammit
+    </body>
+</html>
---
+++ Luke/CodeIgniter-3.1.7/application/views/pages/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+     <a href=pdfs\TimeTablesIct.pdf>link</a>
+    <a href="/ViewerJS/#/pdfs\TimeTablesIct.pdf">link2</a>
+    <br>
+    <iframe src = "/ViewerJS/#/pdfs\TimeTablesIct.pdf" width='400' height='300' allowfullscreen webkitallowfullscreen></iframe>
+    </body>
+</html>
