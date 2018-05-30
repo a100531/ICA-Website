6def94fee5e762bd15f5004e24839dc862b8bbc8
crazymaltese <34515052+crazymaltese@users.noreply.github.com>
2018-05-31 00:41:06 +0200

    trying

--- Luke/CodeIgniter-3.1.7/application/views/pages/home.php
+++ Luke/CodeIgniter-3.1.7/application/views/pages/home.php
@@ -5,9 +5,12 @@
         <title></title>
     </head>
     <body>
-     <a href=pdfs\TimeTablesIct.pdf>link</a>
+
+
+    <a href=pdfs\TimeTablesIct.pdf>link</a>
     <a href="/ViewerJS/#/pdfs\TimeTablesIct.pdf">link2</a>
     <br>
     <iframe src = "/ViewerJS/#/pdfs\TimeTablesIct.pdf" width='400' height='300' allowfullscreen webkitallowfullscreen></iframe>
+
     </body>
 </html>
