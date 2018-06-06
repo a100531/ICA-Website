d8db10906100f0721d6f5d5abab27344a195e861
crazymaltese <34515052+crazymaltese@users.noreply.github.com>
2018-06-06 15:39:13 +0200

    Upadate

--- Luke/CodeIgniter-3.1.7/application/views/upload_success.php
+++ Luke/CodeIgniter-3.1.7/application/views/upload_success.php
@@ -7,12 +7,10 @@
 <h3>Your file was successfully uploaded!</h3>
 
 <ul>
-<?php foreach ($upload_data as $item => $value):?>
-<li><?php echo $item;?>: <?php echo $value;?></li>
-<?php endforeach; ?>
+
 </ul>
 
-<iframe src = "/ViewerJS/#/uploads/<?=$upload_data['file_name']?>" width='400' height='300' allowfullscreen webkitallowfullscreen></iframe>
+<iframe src = "/ViewerJS/#/uploads/test.pdf" width='400' height='300' allowfullscreen webkitallowfullscreen></iframe>
 
 </body>
 </html>
